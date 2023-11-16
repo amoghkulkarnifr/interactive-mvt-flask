db5b41d4e9fdf68c18efcc9d8c5efd0b97d1a308
Amogh Kulkarni <amogh.kulkarni@fradium.com>
2023-11-16 16:44:38 +0530

    WIP: Other compute methods

--- src/utils/mvt/compute.py
+++ src/utils/mvt/compute.py
@@ -7,22 +7,47 @@ import pandas as pd
 
 from src.utils.data import get_company_data
 
-def compute__max_sharpe(company_names: Union[List[str], str]):
-  _data = get_company_data(company_names=company_names)
-
+def __get_efficient_frontier(_data: pd.DataFrame):
   # Calculate expected returns and sample covariance
-  mu = expected_returns.capm_return(_data)
+  mu = expected_returns.ema_historical_return(_data)
   S = risk_models.sample_cov(_data)
 
   # Optimize for maximal Sharpe ratio
   ef = EfficientFrontier(mu, S)
-  weights = ef.max_sharpe()
-  (exp_return, volatility, sharpe_ratio) = ef.portfolio_performance(verbose=True)
+
+  return ef
+
+def compute__max_sharpe(company_names: Union[List[str], str]):
+  _data = get_company_data(company_names=company_names)
+  weights = exp_return = volatility = sharpe_ratio = None
+
+  # Should alwas return True
+  if isinstance(_data, pd.DataFrame):
+    ef = __get_efficient_frontier(_data)
+    weights = ef.max_sharpe()
+    (exp_return, volatility, sharpe_ratio) = ef.portfolio_performance()
 
   return (weights, exp_return, volatility, sharpe_ratio)
 
 def compute__min_volatility(company_names: Union[List[str], str], returns: Optional[float] = 10.0):
-  pass
+  _data = get_company_data(company_names=company_names)
+  weights = exp_return = volatility = sharpe_ratio = None
 
-def compute__max_returns(company_names: Union[List[str], str], volatility: Optional[float] = 10.0):
-  pass
+  # Should alwas return True
+  if isinstance(_data, pd.DataFrame):
+    ef = __get_efficient_frontier(_data)
+    weights = ef.min_volatility()
+    (exp_return, volatility, sharpe_ratio) = ef.portfolio_performance()
+
+  return (weights, exp_return, volatility, sharpe_ratio)
+
+def compute__custom(company_names: Union[List[str], str], weights: List[float]):
+  _data = get_company_data(company_names=company_names)
+  exp_return = volatility = sharpe_ratio = None
+
+  if isinstance(_data, pd.DataFrame):
+    ef = __get_efficient_frontier(_data)
+    # Call ef.set_weights()
+
+  # 
+  return (weights, exp_return, volatility, sharpe_ratio)
